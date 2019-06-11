d54e411d3462b70e03a6255ac8c163ba0c67c6ae
Alexander Hill <alexanderhill4130@gmail.com>
2019-06-11 17:00:14 +0200

    ignore

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :bookings, except: [:show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bookings, only: [:show] do
+  resources :bookings, only: [:show ] do
   resources  :reviews, only: [:new, :create]
   end
 end
