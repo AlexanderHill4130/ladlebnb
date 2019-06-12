74eed5f3c4b4e914f1b3d0f98e7dc276e20b80d2
Mohammed Nouri <simoiroun25@gmail.com>
2019-06-12 16:42:49 +0200

    saved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,10 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initUpdateNavbarOnScroll } from '../plugins/navbar';
+initUpdateNavbarOnScroll();
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
 
 initMapbox();
 
